c93d35b70632d53800d0ad08ee8b112d1891bf50
MagDry <magdalena.drymajlo@gmail.com>
2018-11-05 21:19:59 +0100

    v1

---
+++ js/app.js
@@ -0,0 +1,223 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+//po najechaniu na konkretny numer pojawia się transparentny div z nr i tytułem artykułu
+
+//kiedy kursor najeżdża na ten element:
+
+let nr1 = document.querySelector('.col25:nth-of-type(1)');
+let nr2 = document.querySelector('.col25:nth-of-type(2)');
+let nr3 = document.querySelector('.col25:nth-of-type(3)');
+let nr4 = document.querySelector('.col25:nth-of-type(4)');
+let nr5 = document.querySelector('.col25:nth-of-type(5)');
+let nr6 = document.querySelector('.col25:nth-of-type(6)');
+let nr7 = document.querySelector('.col25:nth-of-type(7)');
+let nr8 = document.querySelector('.col25:nth-of-type(8)');
+let nr9 = document.querySelector('.col25:nth-of-type(9)');
+let nr10 = document.querySelector('.col25:nth-of-type(10)');
+let nr11 = document.querySelector('.col25:nth-of-type(11)');
+let nr12 = document.querySelector('.col25:nth-of-type(12)');
+let nr13 = document.querySelector('.col25:nth-of-type(13)');
+let nr14 = document.querySelector('.col25:nth-of-type(14)');
+let nr15 = document.querySelector('.col25:nth-of-type(15)');
+let nr16 = document.querySelector('.col25:nth-of-type(16)');
+let nr17 = document.querySelector('.col25:nth-of-type(17)');
+
+//console.log(nr1);
+//console.log(nr2);
+
+//w tej części pojawia się klasa .visible
+
+let img1 = document.querySelector('.col25:nth-of-type(1) .transparent');
+let img2 = document.querySelector('.col25:nth-of-type(2) .transparent');
+let img3 = document.querySelector('.col25:nth-of-type(3) .transparent');
+let img4 = document.querySelector('.col25:nth-of-type(4) .transparent');
+let img5 = document.querySelector('.col25:nth-of-type(5) .transparent');
+let img6 = document.querySelector('.col25:nth-of-type(6) .transparent');
+let img7 = document.querySelector('.col25:nth-of-type(7) .transparent');
+let img8 = document.querySelector('.col25:nth-of-type(8) .transparent');
+let img9 = document.querySelector('.col25:nth-of-type(9) .transparent');
+let img10 = document.querySelector('.col25:nth-of-type(10) .transparent');
+let img11 = document.querySelector('.col25:nth-of-type(11) .transparent');
+let img12 = document.querySelector('.col25:nth-of-type(12) .transparent');
+let img13 = document.querySelector('.col25:nth-of-type(13) .transparent');
+let img14 = document.querySelector('.col25:nth-of-type(14) .transparent');
+let img15 = document.querySelector('.col25:nth-of-type(15) .transparent');
+let img16 = document.querySelector('.col25:nth-of-type(16) .transparent');
+let img17 = document.querySelector('.col25:nth-of-type(17) .transparent');
+
+
+//console.log(img4);
+
+//Akcja
+
+//NUMER 1
+
+nr1.addEventListener('mouseover', function() {
+    img1.classList.add('visible');
+});
+
+nr1.addEventListener('mouseout', function() {
+    img1.classList.remove('visible');
+});
+
+//NUMER 2
+
+nr2.addEventListener('mouseover', function() {
+    img2.classList.add('visible');
+});
+
+nr2.addEventListener('mouseout', function() {
+    img2.classList.remove('visible');
+});
+
+//NUMER 3
+
+nr3.addEventListener('mouseover', function() {
+    img3.classList.add('visible');
+});
+
+nr3.addEventListener('mouseout', function() {
+    img3.classList.remove('visible');
+});
+
+//NUMER 4
+
+nr4.addEventListener('mouseover', function() {
+    img4.classList.add('visible');
+});
+
+nr4.addEventListener('mouseout', function() {
+    img4.classList.remove('visible');
+});
+
+//NUMER 5
+
+nr5.addEventListener('mouseover', function() {
+    img5.classList.add('visible');
+});
+
+nr5.addEventListener('mouseout', function() {
+    img5.classList.remove('visible');
+});
+
+//NUMER 6
+
+nr6.addEventListener('mouseover', function() {
+    img6.classList.add('visible');
+});
+
+nr6.addEventListener('mouseout', function() {
+    img6.classList.remove('visible');
+});
+
+//NUMER 7
+
+nr7.addEventListener('mouseover', function() {
+    img7.classList.add('visible');
+});
+
+nr7.addEventListener('mouseout', function() {
+    img7.classList.remove('visible');
+});
+
+//NUMER 8 
+
+nr8.addEventListener('mouseover', function() {
+    img8.classList.add('visible');
+});
+
+nr8.addEventListener('mouseout', function() {
+    img8.classList.remove('visible');
+});
+
+//NUMER 9
+
+nr9.addEventListener('mouseover', function() {
+    img9.classList.add('visible');
+});
+
+nr9.addEventListener('mouseout', function() {
+    img9.classList.remove('visible');
+});
+
+//NUMER 10
+
+nr10.addEventListener('mouseover', function() {
+    img10.classList.add('visible');
+});
+
+nr10.addEventListener('mouseout', function() {
+    img10.classList.remove('visible');
+});
+
+//NUMER 11
+
+nr11.addEventListener('mouseover', function() {
+    img11.classList.add('visible');
+});
+
+nr11.addEventListener('mouseout', function() {
+    img11.classList.remove('visible');
+});
+
+//NUMER 12
+
+nr12.addEventListener('mouseover', function() {
+    img12.classList.add('visible');
+});
+
+nr12.addEventListener('mouseout', function() {
+    img12.classList.remove('visible');
+});
+
+//NUMER 13
+
+nr13.addEventListener('mouseover', function() {
+    img13.classList.add('visible');
+});
+
+nr13.addEventListener('mouseout', function() {
+    img13.classList.remove('visible');
+});
+
+//NUMER 14
+
+nr14.addEventListener('mouseover', function() {
+    img14.classList.add('visible');
+});
+
+nr14.addEventListener('mouseout', function() {
+    img14.classList.remove('visible');
+});
+
+//NUMER 15
+
+nr15.addEventListener('mouseover', function() {
+    img15.classList.add('visible');
+});
+
+nr15.addEventListener('mouseout', function() {
+    img15.classList.remove('visible');
+});
+
+//NUMER 16
+
+nr16.addEventListener('mouseover', function() {
+    img16.classList.add('visible');
+});
+
+nr16.addEventListener('mouseout', function() {
+    img16.classList.remove('visible');
+});
+
+//NUMER 17
+
+nr17.addEventListener('mouseover', function() {
+    img17.classList.add('visible');
+});
+
+nr17.addEventListener('mouseout', function() {
+    img17.classList.remove('visible');
+});
+    
+});
